51cc20bb542fe5dfe047c8e9e01237017bc3091b
Dipen Sherpa <technolove851@gmail.com>
2026-01-09 12:36:54 +0545

    Secure API keys and initial commit

---
+++ config.example.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+    // Google Maps API Key for hotels_restaurants.html
+    // Get your key from: https://console.cloud.google.com/
+    GOOGLE_MAPS_API_KEY: 'YOUR_GOOGLE_MAPS_API_KEY_HERE',
+
+    // OpenWeather API Key for tourist_destination_explorer.html
+    // Get your key from: https://openweathermap.org/api
+    WEATHER_API_KEY: 'YOUR_OPENWEATHER_API_KEY_HERE'
+};
