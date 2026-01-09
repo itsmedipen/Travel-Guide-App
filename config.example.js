3653c98fb498b04db340b88a36accc7c6a326c07
Dipen Sherpa <technolove851@gmail.com>
2026-01-09 12:47:13 +0545

    Update comment in config.example.js

--- config.example.js
+++ config.example.js
@@ -3,7 +3,7 @@ const CONFIG = {
     // Get your key from: https://console.cloud.google.com/
     GOOGLE_MAPS_API_KEY: 'YOUR_GOOGLE_MAPS_API_KEY_HERE',
 
-    // OpenWeather API Key for tourist_destination_explorer.html
+    // OpenWeather API Key for index.html
     // Get your key from: https://openweathermap.org/api
     WEATHER_API_KEY: 'YOUR_OPENWEATHER_API_KEY_HERE'
 };
